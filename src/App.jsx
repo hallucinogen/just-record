1a029b84e54a02abb64c36c49abcd141d7469cae
Listiarso Wastuargo <gogo@Listiarsos-MacBook-Pro.local>
2023-04-03 19:04:57 +0700

    Bajilag

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import play from "./play.svg";
 import stop from "./stop.svg";
-import pause from "./pause.svg";
 import MultiStreamsMixer from "multistreamsmixer";
 
 var videoPreview = document.querySelector("video");
@@ -10,123 +9,6 @@ var mixerOptions = {};
 var mixer;
 var mediaRecorder;
 var recordedBlobs;
-
-function fullCanvasRenderHandler(stream, textToDisplay) {
-  // on-video-render:
-  // called as soon as this video stream is drawn (painted or recorded) on canvas2d surface
-  stream.onRender = function (context, x, y, width, height, idx) {
-    context.font = "50px Georgia";
-    var measuredTextWidth = parseInt(
-      context.measureText(textToDisplay).width
-    );
-    x = x + (parseInt(width - measuredTextWidth) - 40);
-    y = y + 80;
-    context.strokeStyle = "rgb(255, 0, 0)";
-    context.fillStyle = "rgba(255, 255, 0, .5)";
-    roundRect(context, x - 20, y - 55, measuredTextWidth + 40, 75, 20, true);
-    var gradient = context.createLinearGradient(0, 0, width * 2, 0);
-    gradient.addColorStop("0", "magenta");
-    gradient.addColorStop("0.5", "blue");
-    gradient.addColorStop("1.0", "red");
-    context.fillStyle = gradient;
-    context.fillText(textToDisplay, x, y);
-  };
-}
-
-function normalVideoRenderHandler(stream, textToDisplay, callback) {
-  // on-video-render:
-  // called as soon as this video stream is drawn (painted or recorded) on canvas2d surface
-  stream.onRender = function (context, x, y, width, height, idx, ignoreCB) {
-    if (!ignoreCB && callback) {
-      callback(context, x, y, width, height, idx, textToDisplay);
-      return;
-    }
-
-    context.font = "40px Georgia";
-    var measuredTextWidth = parseInt(
-      context.measureText(textToDisplay).width
-    );
-    x = x + parseInt(width - measuredTextWidth) / 2;
-    y = context.canvas.height - height + 50;
-    context.strokeStyle = "rgb(255, 0, 0)";
-    context.fillStyle = "rgba(255, 255, 0, .5)";
-    roundRect(context, x - 20, y - 35, measuredTextWidth + 40, 45, 20, true);
-    var gradient = context.createLinearGradient(0, 0, width * 2, 0);
-    gradient.addColorStop("0", "magenta");
-    gradient.addColorStop("0.5", "blue");
-    gradient.addColorStop("1.0", "red");
-    context.fillStyle = gradient;
-    context.fillText(textToDisplay, x, y);
-  };
-}
-
-/**
- * Draws a rounded rectangle using the current state of the canvas.
- * If you omit the last three params, it will draw a rectangle
- * outline with a 5 pixel border radius
- * @param {CanvasRenderingContext2D} ctx
- * @param {Number} x The top left x coordinate
- * @param {Number} y The top left y coordinate
- * @param {Number} width The width of the rectangle
- * @param {Number} height The height of the rectangle
- * @param {Number} [radius = 5] The corner radius; It can also be an object
- *                 to specify different radii for corners
- * @param {Number} [radius.tl = 0] Top left
- * @param {Number} [radius.tr = 0] Top right
- * @param {Number} [radius.br = 0] Bottom right
- * @param {Number} [radius.bl = 0] Bottom left
- * @param {Boolean} [fill = false] Whether to fill the rectangle.
- * @param {Boolean} [stroke = true] Whether to stroke the rectangle.
- */
-// via: http://stackoverflow.com/a/3368118/552182
-function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
-  if (typeof stroke == "undefined") {
-    stroke = true;
-  }
-  if (typeof radius === "undefined") {
-    radius = 5;
-  }
-  if (typeof radius === "number") {
-    radius = {
-      tl: radius,
-      tr: radius,
-      br: radius,
-      bl: radius,
-    };
-  } else {
-    var defaultRadius = {
-      tl: 0,
-      tr: 0,
-      br: 0,
-      bl: 0,
-    };
-    for (var side in defaultRadius) {
-      radius[side] = radius[side] || defaultRadius[side];
-    }
-  }
-  ctx.beginPath();
-  ctx.moveTo(x + radius.tl, y);
-  ctx.lineTo(x + width - radius.tr, y);
-  ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
-  ctx.lineTo(x + width, y + height - radius.br);
-  ctx.quadraticCurveTo(
-    x + width,
-    y + height,
-    x + width - radius.br,
-    y + height
-  );
-  ctx.lineTo(x + radius.bl, y + height);
-  ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
-  ctx.lineTo(x, y + radius.tl);
-  ctx.quadraticCurveTo(x, y, x + radius.tl, y);
-  ctx.closePath();
-  if (fill) {
-    ctx.fill();
-  }
-  if (stroke) {
-    ctx.stroke();
-  }
-}
 
 function App() {
   const [permissionAllowed, setPermission] = useState(false);
@@ -136,7 +18,6 @@ function App() {
     let handlePermission = async () => {
       mixerOptions.value = "camera-screen";
       let camera = await startCameraAndScreen();
-      console.log(permissionAllowed);
       if (camera === null) {
         setPermission(false);
       } else {
@@ -320,15 +201,16 @@ function App() {
     setStart(true);
   }
 
+  function stopRecording() {
+    mediaRecorder.stop();
+  }
+
+
   function handleDataAvailable(event) {
     console.log("handleDataAvailable", event);
     if (event.data && event.data.size > 0) {
       recordedBlobs.push(event.data);
     }
-  }
-
-  function stopRecording() {
-    mediaRecorder.stop();
   }
 
   async function download() {
@@ -337,7 +219,7 @@ function App() {
     const a = document.createElement("a");
     a.style.display = "none";
     a.href = url;
-    a.download = new Date().getTime() + ".webm";
+    a.download = "Recording-" + new Date().toISOString() + ".webm";
     document.body.appendChild(a);
     a.click();
     setTimeout(() => {
@@ -376,6 +258,7 @@ function App() {
             >
               Your browser does not support the video tag.
             </video>
+            
             <div
               style={{
                 display: "flex",
